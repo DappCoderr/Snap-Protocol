6b50a88d3df1ac7e1a30d384bf01551ca3e6b458
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2022-07-07 18:32:15 +0530

    Update Header component and added context component

--- web/src/App.js
+++ web/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import Example from './components/Example'
 import Footer from './components/Footer'
 import Header from './components/Header'
+
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Example />
       <Footer />
     </div>
   )
--- web/src/components/Header.jsx
+++ web/src/components/Header.jsx
@@ -15,7 +15,7 @@ const Header = () => {
         <div className="pt-16 pb-80 sm:pt-24 sm:pb-40 lg:pt-40 lg:pb-48">
           <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 sm:static">
             <div className="sm:max-w-lg">
-              <h1 className="text-4xl font font-extrabold tracking-tight text-gray-900 sm:text-6xl">
+              <h1 className="text-4xl font font-extrabold tracking-tight text-gray-900 sm:text-8xl">
                    Liquidity Protocol for NFTs
               </h1>
               <p className="mt-4 text-xl text-gray-500">
@@ -91,12 +91,12 @@ const Header = () => {
                   </div>
                 </div>
   
-                <a
+                {/* <a
                   href="#f"
                   className="inline-block text-center bg-indigo-600 border border-transparent rounded-md py-3 px-8 font-medium text-white hover:bg-indigo-700"
                 >
                   Comming Soon
-                </a>
+                </a> */}
               </div>
             </div>
           </div>
